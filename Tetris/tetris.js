cff06b4307a86e925cc2933df732125824aded05
cargo22 <carlosegomez1738@gmail.com>
2023-07-23 17:17:15 -0700

    Update tetris.js

--- Tetris/tetris.js
+++ Tetris/tetris.js
@@ -75,49 +75,28 @@ document.addEventListener('DOMContentLoaded', () => {
     })
   }
 
-  // making an outline
+  // making an outline piece that shows where the piece will end up
   function drawGhostPiece() {
-    // Reset ghostPosition to the initial position
-    ghostPosition = currentPosition;
+    ghostPosition = currentPosition
 
-    // Move the ghost outline up to the first available block
+    // making sure the outlined piece moves up if piece is already there
     while (!isCollision(ghostPiece, ghostPosition)) {
-      ghostPosition += width;
+      ghostPosition += width
     }
 
-    // Move ghostPosition back down one row to the last valid position
-    ghostPosition += width;
+    // move ghostPosition back down one row to the last valid position
+    ghostPosition += width
 
-    // Draw the ghost outline within the visible grid area
+    // draw the ghost outline within the visible grid area
     if (ghostPosition >= 0) {
       ghostPiece.forEach(index => {
         if (ghostPosition + index < squares.length) {
-          squares[ghostPosition + index].classList.add('ghost');
+          squares[ghostPosition + index].classList.add('ghost')
         }
       });
     }
 
-    updateCustomColor(random);
-    // while (!isCollision(ghostPiece, ghostPosition)) {
-    //   ghostPosition += width
-    // }
-
-    // let max = 0
-    // for (let index = 0; index < ghostPiece.length; index++) {
-    //   if (ghostPiece[index] > max) {
-    //     max = ghostPiece[index]
-    //   }
-    // }
-
-    // if (max + ghostPosition < squares.length - width * 2) {
-    //   ghostPosition += width
-    // }
-
-    // ghostPiece.forEach(index => {
-    //   squares[ghostPosition + index].classList.add('ghost')
-    // })
-
-    // updateCustomColor(random)
+    updateCustomColor(random)
   }
 
   // undraw the block
@@ -139,57 +118,55 @@ document.addEventListener('DOMContentLoaded', () => {
   // assign functions to keyCodes
   // ------------------------------------------------------------------------------------------------
   function control(e) {
+    // left arrow
     if (e.keyCode == 37) {
       moveLeft()
       moveLeftGhost()
+      // right arrow
     } else if (e.keyCode == 39) {
       moveRight()
       moveRightGhost()
+      // up arrow  
     } else if (e.keyCode == 38) {
       rotate()
       rotateGhostPiece()
+      // down arrow  
     } else if (e.keyCode == 40) {
       moveDown()
+      // space bar
     } else if (e.keyCode == 32) {
       while (!isCollision(current, currentPosition)) {
-        moveDown();
+        moveDown()
       }
       moveDown()
       undrawGhostPiece()
     }
   }
-  document.addEventListener('keyup', control)
-  let escapePressed = false; // Flag to track if the escape key was pressed
 
-  // Event listener to handle the escape key press
+  document.addEventListener('keyup', control)
+
+  // programming a pause screen
+  // ------------------------------------------------------------------------------------------------
+  let escapePressed = false
+
   document.addEventListener('keyup', (e) => {
-    if (e.key === 'Escape') { // Check if the Esc key was pressed
+    if (e.key === 'Escape') {
+      // checking if the pause screen is already there
       if (timerID) {
-        clearInterval(timerID);
-        timerID = null;
-        isPaused = true; // Set the paused state to true
-        showPauseScreen(); // Show the pause screen
-        // Re-add the space key event listener to handle manual piece movement
-        document.addEventListener('keyup', spaceKeyListener);
-        escapePressed = true; // Set the flag to true since escape was pressed
+        clearInterval(timerID)
+        timerID = null
+        isPaused = true
+        showPauseScreen()
+        escapePressed = true
       } else {
-        if (isPaused) { // Only resume if the game is currently paused
-          timerID = setInterval(moveDown, 1000);
-          isPaused = false; // Set the paused state to false
-          hidePauseScreen(); // Hide the pause screen
-          // Remove the space key event listener since the game is running again
-          document.removeEventListener('keyup', spaceKeyListener);
+        if (isPaused) {
+          timerID = setInterval(moveDown, 1000)
+          isPaused = false
+          hidePauseScreen()
         }
-        escapePressed = false; // Set the flag to false since escape was released
+        escapePressed = false
       }
     }
-    // } else if (e.key === ' ' && !escapePressed) { // Check if the space key was pressed
-    //   while (!isCollision(current, currentPosition)) {
-    //     moveDown();
-    //   }
-    //   moveDown();
-    //   undrawGhostPiece();
-    // }
   });
 
   // movement
@@ -212,32 +189,31 @@ document.addEventListener('DOMContentLoaded', () => {
       currentPosition += 1
     }
 
-
     draw()
   }
 
   function moveLeftGhost() {
-    undrawGhostPiece();
+    undrawGhostPiece()
 
-    const isAtLeftEdge = ghostPiece.some(index => (ghostPosition + index) % width === 0);
+    const isAtLeftEdge = ghostPiece.some(index => (ghostPosition + index) % width === 0)
 
     if (!isAtLeftEdge) {
-      ghostPosition -= 1;
+      ghostPosition -= 1
     }
 
     if (ghostPiece.some(index => squares[ghostPosition + index].classList.contains('taken'))) {
-      // If collision occurs, move up to the first available block
       while (ghostPiece.some(index => squares[ghostPosition + index + width].classList.contains('taken'))) {
-        ghostPosition -= width;
+        // moving piece up until the first available square
+        ghostPosition -= width
       }
     }
 
-    // If after moving up, there's a collision again, move back down one row
+    
     while (ghostPiece.some(index => squares[ghostPosition + index].classList.contains('taken'))) {
-      ghostPosition += width;
+      ghostPosition += width
     }
 
-    drawGhostPiece();
+    drawGhostPiece()
   }
 
   function moveRight() {
@@ -257,27 +233,26 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   function moveRightGhost() {
-    undrawGhostPiece();
+    undrawGhostPiece()
 
-    const isAtRightEdge = ghostPiece.some(index => (ghostPosition + index) % width === width - 1);
+    const isAtRightEdge = ghostPiece.some(index => (ghostPosition + index) % width === width - 1)
 
     if (!isAtRightEdge) {
-      ghostPosition += 1;
+      ghostPosition += 1
     }
 
     if (ghostPiece.some(index => squares[ghostPosition + index].classList.contains('taken'))) {
-      // If collision occurs, move up to the first available block
       while (ghostPiece.some(index => squares[ghostPosition + index + width].classList.contains('taken'))) {
-        ghostPosition -= width;
+        // moving piece up until the first available square
+        ghostPosition -= width
       }
     }
 
-    // If after moving up, there's a collision again, move back down one row
     while (ghostPiece.some(index => squares[ghostPosition + index].classList.contains('taken'))) {
-      ghostPosition += width;
+      ghostPosition += width
     }
 
-    drawGhostPiece();
+    drawGhostPiece()
   }
 
   function rotate() {
@@ -294,25 +269,26 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   function rotateGhostPiece() {
-    undrawGhostPiece();
+    undrawGhostPiece()
 
-    ghostRotation++;
+    ghostRotation++
     if (ghostRotation === ghostPiece.length) {
-      ghostRotation = 0;
+      ghostRotation = 0
     }
 
-    const nextGhostPiece = blocks[random][ghostRotation];
+    const nextGhostPiece = blocks[random][ghostRotation]
 
-    // Check for collisions when rotating and adjust the position upward if necessary
+    // checking for collisions when rotating and adjust the position upward if necessary
     while (isCollision(nextGhostPiece, ghostPosition)) {
-      ghostPosition -= width;
+      ghostPosition -= width
     }
 
-    ghostPiece = nextGhostPiece;
-    drawGhostPiece();
+    ghostPiece = nextGhostPiece
+    drawGhostPiece()
   }
-  // ------------------------------------------------------------------------------------------------
+
   // checking if rotations can be made. if not, we are making sure it doesn't wrap around to the other end
+  // ------------------------------------------------------------------------------------------------
 
   function isAtRight() {
     return current.some(index => (currentPosition + index + 1) % width === 0)
@@ -322,12 +298,14 @@ document.addEventListener('DOMContentLoaded', () => {
     return current.some(index => (currentPosition + index) % width === 0)
   }
 
+  // checking to see if the rotation can be made within the grid's width. if not, rotate in a way where the block
+  // does not wrap around the grid
   function checkRotatedPosition(P) {
-    P = P || currentPosition       //get current position.  Then, check if the piece is near the left side.
-    if ((P + 1) % width < 4) {         //add 1 because the position index can be 1 less than where the piece is (with how they are indexed).     
-      if (isAtRight()) {            //use actual position to check if it's flipped over to right side
-        currentPosition += 1    //if so, add one to wrap it back around
-        checkRotatedPosition(P) //check again.  Pass position from start, since long block might need to move more.
+    P = P || currentPosition       
+    if ((P + 1) % width < 4) {             
+      if (isAtRight()) {           
+        currentPosition += 1    
+        checkRotatedPosition(P) 
       }
     }
     else if (P % width > 5) {
@@ -348,11 +326,11 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   function checkRotatedPositionGhost(P) {
-    P = P || ghostPosition       //get current position.  Then, check if the piece is near the left side.
-    if ((P + 1) % width < 4) {         //add 1 because the position index can be 1 less than where the piece is (with how they are indexed).     
-      if (isAtRightGhost()) {            //use actual position to check if it's flipped over to right side
-        ghostPosition += 1    //if so, add one to wrap it back around
-        checkRotatedPosition(P) //check again.  Pass position from start, since long block might need to move more.
+    P = P || ghostPosition       
+    if ((P + 1) % width < 4) {         
+      if (isAtRightGhost()) {           
+        ghostPosition += 1    
+        checkRotatedPosition(P) 
       }
     }
     else if (P % width > 5) {
@@ -363,8 +341,8 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   }
 
-  // ------------------------------------------------------------------------------------------------
   // helper functions
+  // ------------------------------------------------------------------------------------------------
 
   function isCollision(type, position) {
     return type.some((index) =>
@@ -390,15 +368,27 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
 
-  var root = document.documentElement;
+  var root = document.documentElement
 
   function updateCustomColor(random) {
-    // Update the CSS variable value
-    root.style.setProperty('--custom-color', colors[random]);
+    // update the CSS variable value to be the same as the block color
+    root.style.setProperty('--custom-color', colors[random])
   }
 
-  // ------------------------------------------------------------------------------------------------
+  // functions for the pause screen
+  function showPauseScreen() {
+    const pauseScreen = document.getElementById('pauseScreen')
+    pauseScreen.style.display = 'block'
+  }
+
+  function hidePauseScreen() {
+    const pauseScreen = document.getElementById('pauseScreen')
+    pauseScreen.style.display = 'none'
+  }
+
   // show the next block in rotation
+  // ------------------------------------------------------------------------------------------------
+
   const displaySquares = document.querySelectorAll('.mini-grid div')
   const displayWidth = 4
   const displayIndex = 0
@@ -470,17 +460,5 @@ document.addEventListener('DOMContentLoaded', () => {
       clearInterval(timerID)
     }
   }
-
-  function showPauseScreen() {
-    const pauseScreen = document.getElementById('pauseScreen');
-    pauseScreen.style.display = 'block';
-  }
-
-  // Function to hide the pause screen
-  function hidePauseScreen() {
-    const pauseScreen = document.getElementById('pauseScreen');
-    pauseScreen.style.display = 'none';
-  }
-
 
 })
