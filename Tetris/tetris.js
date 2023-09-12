3fc8f4a1fdcbccbde0f005f7f05536e4b323f59f
cargo22 <carlosegomez1738@gmail.com>
2023-09-11 20:28:20 -0700

    making it look nice

--- Tetris/tetris.js
+++ Tetris/tetris.js
@@ -5,7 +5,6 @@ document.addEventListener('DOMContentLoaded', () => {
   let squares = Array.from(document.querySelectorAll('.grid div'))
   const scoreDisplay = document.querySelector('#score')
   const levelDisplay = document.querySelector('#level')
-  const pauseMessage = document.querySelector('#pauseMessage')
   const startButton = document.querySelector('#startButton')
   let nextRandom = 0
   const width = 10
@@ -22,7 +21,7 @@ document.addEventListener('DOMContentLoaded', () => {
     'cyan'
   ]
 
-  // drawing the blocks
+  // drawing the blocks. each different list is a rotated form of the original block
   // ------------------------------------------------------------------------------------------------
   const lBlock = [
     [1, width + 1, width * 2 + 1, 2],
@@ -119,7 +118,7 @@ document.addEventListener('DOMContentLoaded', () => {
     updateCustomColor(random)
   }
 
-  // undraw the block
+  // undraw (erase) the block
   function undraw() {
     current.forEach(index => {
       squares[currentPosition + index].classList.remove('block')
@@ -127,7 +126,7 @@ document.addEventListener('DOMContentLoaded', () => {
     })
   }
 
-  // undraw the outline
+  // undraw the outlined piece
   function undrawGhostPiece() {
     ghostPiece.forEach(index => {
       squares[ghostPosition + index].classList.remove('ghost')
@@ -169,7 +168,7 @@ document.addEventListener('DOMContentLoaded', () => {
   let escapePressed = false
 
   document.addEventListener('keyup', (e) => {
-    if (e.key === 'Escape') {
+    if (e.key == 'Escape') {
       // checking if the pause screen is already there
       if (timerID) {
         clearInterval(timerID)
@@ -193,6 +192,7 @@ document.addEventListener('DOMContentLoaded', () => {
   //programming a reset button
   // ------------------------------------------------------------------------------------------------
 
+  // function that starts a brand new game from scratch
   function resetGame() {
     // removing all blocks from the grid
     squares.slice(0, 200).forEach(square => {
@@ -202,6 +202,7 @@ document.addEventListener('DOMContentLoaded', () => {
       square.style.backgroundColor = ''
     });
 
+    // making sure audio restarts
     startButton.disabled = false
     audio.currentTime = 0
     audio.play()
@@ -213,7 +214,7 @@ document.addEventListener('DOMContentLoaded', () => {
       hidePauseScreen()
       scoreDisplay.innerHTML = score
     } else {
-      // resetting all values
+      // resetting all values to start fresh
       currentPosition = 4
       currentRotation = 0
       ghostPosition = 4
@@ -233,11 +234,13 @@ document.addEventListener('DOMContentLoaded', () => {
       hidePauseScreen()
 
       // start game again
-      timerID = setInterval(moveDown, fallSpeed);
+      timerID = setInterval(moveDown, fallSpeed)
     }
 
   }
 
+  // this function is the same as the one above, but this one is specifically for the restart button that is pressed when the 
+  // game is over
   function resetGameFinal() {
     // removing all blocks from the grid
     squares.slice(0, 200).forEach(square => {
@@ -278,12 +281,17 @@ document.addEventListener('DOMContentLoaded', () => {
       hideFinalScreen()
 
       // start game again
-      timerID = setInterval(moveDown, fallSpeed);
+      timerID = setInterval(moveDown, fallSpeed)
     }
 
   }
+
+  // applying the functions when the buttons are pressed
+  // ------------------------------------------------------------------------------------------------
   const resetButton = document.getElementById('restartButton')
   const resumeButton = document.getElementById('resumeButton')
+
+  // giving function to the restart and resume buttons
   resetButton.addEventListener('click', resetGame)
   resumeButton.addEventListener('click', () => {
     hidePauseScreen()
@@ -294,24 +302,28 @@ document.addEventListener('DOMContentLoaded', () => {
     startButton.disabled = false
   })
 
+  // programming button on game over screen
   const finalRestartButton = document.getElementById('freshRestart')
   finalRestartButton.addEventListener('click', resetGameFinal)
 
   // movement
   // ------------------------------------------------------------------------------------------------
+
+  // this function moves the block down one row
   function moveDown() {
     if (lockingDelay) {
-      return; // Prevent moving down during locking delay
+      return
     }
 
-    undraw();
-    currentPosition += width;
-    draw();
-    freeze();
+    undraw()
+    currentPosition += width
+    draw()
+    freeze()
     gameOver()
   }
 
-
+  // this function is the same as the one above, it just doesn't have a locking delay. this function
+  // is specifically meant for when the space bar is pressed
   function moveDownSpace() {
     undraw()
     undrawGhostPiece()
@@ -321,27 +333,33 @@ document.addEventListener('DOMContentLoaded', () => {
     freezeSpace()
   }
 
+  // this function moves the block one tile to the left
   function moveLeft() {
     undraw()
 
+    const isAtLeftEdge = current.some(index => (currentPosition + index) % width === 0)
 
-    const isAtLeftEdge = current.some(index => (currentPosition + index) % width === 0);
+    // making sure the block can't move out of range
     if (!isAtLeftEdge) {
-      currentPosition -= 1;
+      currentPosition -= 1
     }
+
     if (current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
-      currentPosition += 1;
+      currentPosition += 1
     }
 
 
     draw()
   }
 
+  // this function moves the outlined piece one to the left when the normal piece moves left
+  // as well
   function moveLeftGhost() {
     undrawGhostPiece()
 
     const isAtLeftEdge = ghostPiece.some(index => (ghostPosition + index) % width === 0)
 
+    // making sure the block can't move out of range
     if (!isAtLeftEdge) {
       ghostPosition -= 1
     }
@@ -353,7 +371,6 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     }
 
-
     while (ghostPiece.some(index => squares[ghostPosition + index].classList.contains('taken'))) {
       ghostPosition += width
     }
@@ -361,22 +378,25 @@ document.addEventListener('DOMContentLoaded', () => {
     drawGhostPiece()
   }
 
+  // this function moves the block one tile to the right
   function moveRight() {
     undraw()
 
-    const isAtRightEdge = current.some(index => (currentPosition + index) % width === width - 1);
+    const isAtRightEdge = current.some(index => (currentPosition + index) % width === width - 1)
+
+    // making sure the block doesn't move out of range
     if (!isAtRightEdge) {
-      currentPosition += 1;
+      currentPosition += 1
     }
+
     if (current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
-      currentPosition -= 1;
+      currentPosition -= 1
     }
-
-
 
     draw()
   }
 
+  // same as the one above, but for the outlined piece
   function moveRightGhost() {
     undrawGhostPiece()
 
@@ -400,15 +420,22 @@ document.addEventListener('DOMContentLoaded', () => {
     drawGhostPiece()
   }
 
+  // this function rotates the piece to the next available list the block presents
   function rotate() {
     undraw()
+
+    // keeping current rotation
     let prevRotation = currentRotation
     currentRotation++
+
+    // wrapping around the lists, so there's no out of bounds error
     if (currentRotation == current.length) {
       currentRotation = 0
     }
 
     const newCurrent = blocks[random][currentRotation]
+
+    // making sure rotate isn't possible if a collision is happening
     if (!isCollision(current, currentPosition)) {
       current = newCurrent
     } else {
@@ -419,7 +446,7 @@ document.addEventListener('DOMContentLoaded', () => {
     draw()
   }
 
-
+  // same as above, but this one rotates the outlined piece
   function rotateGhostPiece() {
     undrawGhostPiece()
     let prevPiece = ghostPiece
@@ -428,13 +455,16 @@ document.addEventListener('DOMContentLoaded', () => {
     if (ghostRotation === ghostPiece.length) {
       ghostRotation = 0
     }
-    
+
     while (isCollision(ghostPiece, ghostPosition)) {
-    // checking for collisions when rotating and adjust the position upward if necessary
+      // checking for collisions when rotating and adjust the position upward if necessary
       ghostPosition -= width
     }
+
     const newGhostPiece = blocks[random][ghostRotation]
-    if(!isCollision(current, currentPosition)){
+
+    // if there's a collision with the actual block, the outlined piece won't change either
+    if (!isCollision(current, currentPosition)) {
       ghostPiece = newGhostPiece
     } else {
       ghostPiece = prevPiece
@@ -446,12 +476,23 @@ document.addEventListener('DOMContentLoaded', () => {
   // checking if rotations can be made. if not, we are making sure it doesn't wrap around to the other end
   // ------------------------------------------------------------------------------------------------
 
+  // checking to see if the block is at the right edge of the grid
   function isAtRight() {
     return current.some(index => (currentPosition + index + 1) % width === 0)
   }
 
+  // checking to see if the block is at the left edge of the grid
   function isAtLeft() {
     return current.some(index => (currentPosition + index) % width === 0)
+  }
+
+  // doing same for the outlined piece
+  function isAtRightGhost() {
+    return ghostPiece.some(index => (ghostPosition + index + 1) % width === 0)
+  }
+
+  function isAtLeftGhost() {
+    return ghostPiece.some(index => (ghostPosition + index) % width === 0)
   }
 
   // checking to see if the rotation can be made within the grid's width. if not, rotate in a way where the block
@@ -470,15 +511,6 @@ document.addEventListener('DOMContentLoaded', () => {
         checkRotatedPosition(P)
       }
     }
-  }
-
-  // doing same for the outlined piece
-  function isAtRightGhost() {
-    return ghostPiece.some(index => (ghostPosition + index + 1) % width === 0)
-  }
-
-  function isAtLeftGhost() {
-    return ghostPiece.some(index => (ghostPosition + index) % width === 0)
   }
 
   function checkRotatedPositionGhost(P) {
@@ -500,6 +532,7 @@ document.addEventListener('DOMContentLoaded', () => {
   // helper functions
   // ------------------------------------------------------------------------------------------------
 
+  // checking to see if there is a collision with an already established block.
   function isCollision(type, position) {
     return type.some((index) => {
       const nextIndex = position + index + width * 2
@@ -513,16 +546,19 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   }
 
-
   let lockingDelay = false
 
+  // function that is called when a block reaches the last available position
   function freeze() {
+    // checking if block is in contact with a 'taken' block
     if (current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
-      const isAtBottom = current.some(index => (currentPosition + index) + width * 2 >= 200);
+      const isAtBottom = current.some(index => (currentPosition + index) + width * 2 >= 200)
 
+      // setting a delay so that user can move left and right before piece locks in place
       if (isAtBottom || current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
-        lockingDelay = true;
+        lockingDelay = true
 
+        // setting delay to 300ms
         const delayLock = setTimeout(() => {
           clearTimeout(delayLock)
           lockingDelay = false
@@ -539,6 +575,7 @@ document.addEventListener('DOMContentLoaded', () => {
           } else if (e.key === 'ArrowRight') {
             moveRight()
           }
+
         }, { once: true })
       } else {
         lockPiece()
@@ -546,7 +583,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   }
 
-
+  // freeze function meant for when the space bar is pressed. no delay
   function freezeSpace() {
     if (current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
       current.forEach(index => squares[currentPosition + index].classList.add('taken'))
@@ -571,9 +608,7 @@ document.addEventListener('DOMContentLoaded', () => {
     drawGhostPiece()
   }
 
-
-
-
+  // the function that locks the block in place
   function lockPiece() {
     current.forEach(index => squares[currentPosition + index].classList.add('taken'))
     undrawGhostPiece()
@@ -591,16 +626,17 @@ document.addEventListener('DOMContentLoaded', () => {
     addScore()
   }
 
-
-
   var root = document.documentElement
 
+  // this changes the colors of the blocks
   function updateCustomColor(random) {
     // update the CSS variable value to be the same as the block color
     root.style.setProperty('--custom-color', colors[random])
   }
 
-  // functions for the pause screen
+  // functions for the different screen scenarios
+  // ------------------------------------------------------------------------------------------------
+
   function showPauseScreen() {
     const pauseScreen = document.getElementById('pauseScreen')
     pauseScreen.style.display = 'block'
@@ -620,6 +656,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const finalScreen = document.getElementById('resultScreen')
     finalScreen.style.display = 'none'
   }
+
   // show the next block in rotation
   // ------------------------------------------------------------------------------------------------
 
@@ -638,6 +675,7 @@ document.addEventListener('DOMContentLoaded', () => {
     [1, displayWidth, displayWidth + 1, displayWidth * 2]
   ]
 
+  // function to display the upcoming shape in the mini grid to the side
   function displayShape() {
     displaySquares.forEach(square => {
       square.classList.remove('block')
@@ -680,27 +718,32 @@ document.addEventListener('DOMContentLoaded', () => {
     startButton.disabled = false
   })
 
+  // function to make sure the score is calculated correctly
   function addScore() {
     for (let i = 0; i < 199; i += width) {
       const row = [i, i + 1, i + 2, i + 3, i + 4, i + 5, i + 6, i + 7, i + 8, i + 9]
 
+      // checking if every block in the row is 'taken'
       if (row.every(index => squares[index].classList.contains('taken'))) {
         addLevel()
         score += 10
         scoreDisplay.innerHTML = score
 
+        // removes the row
         current.forEach(index => {
           squares[currentPosition + index].classList.remove('block')
           squares[currentPosition + index].style.backgroundColor = ''
         })
 
-
+        // removes every block and sets it to the original color
         row.forEach(index => {
           squares[index].classList.remove('taken')
           squares[index].classList.remove('block')
           squares[index].classList.remove('ghost')
           squares[index].style.backgroundColor = ''
         })
+
+        // adding deleted row to the top so that we don't lose any rows
         const squaresRemoved = squares.splice(i, width)
         squares = squaresRemoved.concat(squares)
         squares.forEach(cell => grid.appendChild(cell))
@@ -710,12 +753,12 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   }
 
+  // function that determines what happens when the game is over
   function gameOver() {
-    console.log('Game Over function called');
     if (current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
-      clearInterval(timerID);
-      timerID = null;
-      showFinalScreen();
+      clearInterval(timerID)
+      timerID = null
+      showFinalScreen()
       audio.pause()
     }
   }
@@ -727,6 +770,7 @@ document.addEventListener('DOMContentLoaded', () => {
   let currentLevel = 0
   levelDisplay.innerHTML = currentLevel
 
+  // function that updates the level depending on the score and also speeds up the falling time
   function addLevel() {
     if (score % 100 == 0) {
       currentLevel++
