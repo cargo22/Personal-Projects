7e9c76e1aa2fe495fd16f081f9d5cfdea9eb14d5
cargo22 <carlosegomez1738@gmail.com>
2025-09-16 13:24:58 -0700

    getting tournament information

--- Fifa Stat Tracker/script.js
+++ Fifa Stat Tracker/script.js
@@ -1,8 +1,22 @@
 const start = document.getElementById("start-button");
 const initial_screen = document.getElementById("start-screen");
 const setup_screen = document.getElementById("setup-screen");
+const players_select = document.getElementById("num-players");
+const advance = document.getElementById("advance-button");
+const tournament_hub = document.getElementById("tournament-hub");
 
 start.addEventListener("click", () => {
     initial_screen.style.display = "none";
     setup_screen.style.display = "flex";
+});
+
+let num_players = players_select.value;
+
+players_select.addEventListener('change', function() {
+    num_players = this.value;
+});
+
+advance.addEventListener("click", () => {
+    setup_screen.style.display = "none";
+    tournament_hub.style.display = "flex";
 });
